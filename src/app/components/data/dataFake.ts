6e6d2619c8bc8a14d65a762dbb519c3895145d78
Edvaldo José <edvaldojose003@gmail.com>
2023-10-09 20:04:26 -0300

    Dynamic content update

--- src/app/components/data/dataFake.ts
+++ src/app/components/data/dataFake.ts
@@ -14,13 +14,37 @@ export const dataFake = [
   {
     "id": "3",
     "title": "mais um novo filme",
-    "description": "Ola lancamento3 novidades!",
+    "description": "Ola temos lancamentos e novidades!",
     "photoCover": "https://midias.correiobraziliense.com.br/_midias/jpg/2022/11/09/1200x800/1_pantera_negra_wakanda_para_sempre-26819084.jpg?20221109143843?20221109143843"
   },
   {
     "id": "4",
     "title": "mais um novo filme",
-    "description": "Ola lancamentos novidades!",
+    "description": "Ola temos lancamentos e novidades!",
     "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNZJKY8OL8rk1NKI5hE2_GsGfg7J1pP7nsdin5orBvhbn_Jkss"
+  },
+  {
+    "id": "5",
+    "title": "saiu o novo filme",
+    "description": "Ola temos novidades!",
+    "photoCover": "https://cdn.ome.lt/mInMVqN27TZqBu6zoZFxf_uNNNs=/1200x630/smart/extras/conteudos/2019-08-12-OMELETV-DQ.png"
+  },
+  {
+    "id": "6",
+    "title": "mais um novo filme",
+    "description": "Nova filme Avengers anuciada chegou a vez das mulheres.",
+    "photoCover": "https://i.ytimg.com/vi/vC78swLurYw/maxresdefault.jpg"
+  },
+  {
+    "id": "7",
+    "title": "mais um novo filme",
+    "description": "Sera que o Homem de Ferro morreu mesmo!!",
+    "photoCover": "https://i0.statig.com.br/bancodeimagens/4i/x8/bl/4ix8bl7zlpqsa4b3r9myay3oh.jpg"
+  },
+  {
+    "id": "8",
+    "title": "mais um novo filme",
+    "description": "Excelente atriz seria uma boa se ela fosse escolhida!",
+    "photoCover": "https://d17lbu6bbzbdc8.cloudfront.net/wp-content/uploads/2022/07/13114835/Novo-traje-Ironheart-do-Homem-de-Ferro-do-MCU-revelado-em-Pantera-Negra-2.jpg"
   }
 ]
