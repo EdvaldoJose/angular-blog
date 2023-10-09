6e9eea9a2bfc65891115470bdd747b3f3c1e946f
Edvaldo José <edvaldojose003@gmail.com>
2023-10-08 21:43:44 -0300

    Deixando content danamycs

---
+++ src/app/components/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "saiu o novo filme",
+    "description": "Ola temos novidades!",
+    "photoCover": "https://observatoriodegames.uol.com.br/wp-content/uploads/2020/08/MArvels-Avengers-1.png"
+  },
+  {
+    "id": "2",
+    "title": "mais um novo filme",
+    "description": "Ola lancamento novidades!",
+    "photoCover": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSTn-jbUj3NveLkXHnC9bas-leqifdxv8TRxnY5VZYJyy_WBm1T"
+  },
+  {
+    "id": "3",
+    "title": "mais um novo filme",
+    "description": "Ola lancamento3 novidades!",
+    "photoCover": "https://midias.correiobraziliense.com.br/_midias/jpg/2022/11/09/1200x800/1_pantera_negra_wakanda_para_sempre-26819084.jpg?20221109143843?20221109143843"
+  },
+  {
+    "id": "4",
+    "title": "mais um novo filme",
+    "description": "Ola lancamento3 novidades!",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNZJKY8OL8rk1NKI5hE2_GsGfg7J1pP7nsdin5orBvhbn_Jkss"
+  }
+]
